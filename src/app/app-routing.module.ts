50842ec61edef81688e3b3515282e8365bd6790d
Onkel Toob <info@tobias-reinhardt.de>
2017-09-27 22:38:28 +0200

    Einzelseite vorbereitet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule }              from '@angular/core';
 import { RouterModule, Routes }  from '@angular/router';
  
 import { HomeComponent }   from './component/home/home.component';
+// import { MixComponent }   from './component/mix/mix.component';
 import { RouterExampleComponent }     from './component/router-example/router-example.component';
 import { NotFoundComponent } from './component/not-found/not-found.component';
 
 const appRoutes: Routes = [
     { path: 'router-example', component: RouterExampleComponent },
     { path: 'music', component: HomeComponent },
+    // { path: 'music/:title', component: MixComponent },
     { path: '',   redirectTo: '/music', pathMatch: 'full' },
     { path: '**', component: NotFoundComponent }
   ];
